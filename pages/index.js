b27050577898cb16131f2c943c8cbe5bec7e623d
Jason Herring <jason@ordinarykids.com>
2023-10-23 20:32:22 -0700

    bump

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <a href="https://www.aleph.dev"><Image src="/ok2.png" alt="me" width="287.5" height="318" /></a>
+    **
+   
     </div>
   ) 
 }
